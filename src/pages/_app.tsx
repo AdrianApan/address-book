c436aeb0c90c02762e121e98b3e986319b256ece
Adrian Apan <mail@adrianapan.com>
2023-04-19 16:56:15 +0100

    ✨ add react query provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,29 @@ import 'src/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { createTheme, ThemeProvider } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const theme = createTheme()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <CssBaseline />
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   )
