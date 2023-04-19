c0a125a9df43d1faf800e06fd651059793f22fea
Adrian Apan <mail@adrianapan.com>
2023-04-19 11:44:18 +0100

    ♻️ refactor head component

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,19 +1,17 @@
 import { default as NextHead } from 'next/head'
 
 interface Props {
-  name?: string
+  title?: string
   description?: string
 }
 
 const DEFAULT_TITLE = 'Address book'
 const DEFAULT_DESCRIPTION = 'An address book app built with Next JS'
 
-const Head = ({ name, description }: Props) => {
+const Head = ({ title, description }: Props) => {
   return (
     <NextHead>
-      <title>
-        {name && `${name} | `} {DEFAULT_TITLE}
-      </title>
+      <title>{title || DEFAULT_TITLE}</title>
       <meta name="description" content={description || DEFAULT_DESCRIPTION} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
