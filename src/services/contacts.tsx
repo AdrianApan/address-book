6e089e3d881c1017116070d20cf29911a2506121
Adrian Apan <mail@adrianapan.com>
2023-04-19 18:14:52 +0100

    ♻️ update api file

--- src/services/contacts.tsx
+++ src/services/contacts.tsx
@@ -7,16 +7,16 @@ export const getContacts = async () => {
 }
 
 export const addContact = async (data: Partial<Contact>) => {
-  const response = await apiClient.post<Contact[]>('/api/v1/contacts', { data })
+  const response = await apiClient.post<Contact>('/api/v1/contacts', { ...data })
   return response.data
 }
 
 export const editContact = async (id: Contact['id'], data: Partial<Contact>) => {
-  const response = await apiClient.put<Contact[]>(`/api/v1/contacts/${id}`, { data })
+  const response = await apiClient.put<Contact>(`/api/v1/contacts/${id}`, { ...data })
   return response.data
 }
 
 export const deleteContact = async (id: Contact['id']) => {
-  const response = await apiClient.delete<Contact[]>(`/api/v1/contacts/${id}`)
+  const response = await apiClient.delete<Contact>(`/api/v1/contacts/${id}`)
   return response.data
 }
