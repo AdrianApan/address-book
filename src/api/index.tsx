18faa3a0360feae1fa8d4b5c7cb7ea97f11cf5a4
Adrian Apan <mail@adrianapan.com>
2023-04-19 16:55:38 +0100

    ♻️ refactor api file

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,16 +1,31 @@
 import axios from 'axios'
 
-import { ContactID, ContactData, ContactDataResponse } from 'src/types/Contacts'
+import { Contact } from 'src/types/Contact'
 import { API_BASE_URL } from 'src/utils/consts'
 
-export const getContacts = (): Promise<ContactDataResponse> =>
-  axios.get(`${API_BASE_URL}/api/v1/contacts`)
+const apiClient = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-type': 'application/json',
+  },
+})
 
-export const createContact = (data: ContactData): Promise<ContactDataResponse> =>
-  axios.post(`${API_BASE_URL}/api/v1/contacts`, data)
+export const getContacts = async () => {
+  const response = await apiClient.get<Contact[]>('/api/v1/contacts')
+  return response.data
+}
 
-export const editContact = (id: ContactID, data: ContactData): Promise<ContactDataResponse> =>
-  axios.put(`${API_BASE_URL}/api/v1/contacts/${id}`, data)
+export const addContact = async (data: Partial<Contact>) => {
+  const response = await apiClient.post<Contact[]>('/api/v1/contacts', { data })
+  return response.data
+}
 
-export const deleteContact = (id: ContactID): Promise<ContactDataResponse> =>
-  axios.delete(`${API_BASE_URL}/api/v1/contacts/${id}`)
+export const editContact = async (id: Contact['id'], data: Partial<Contact>) => {
+  const response = await apiClient.put<Contact[]>(`/api/v1/contacts/${id}`, { data })
+  return response.data
+}
+
+export const deleteContact = async (id: Contact['id']) => {
+  const response = await apiClient.delete<Contact[]>(`/api/v1/contacts/${id}`)
+  return response.data
+}
