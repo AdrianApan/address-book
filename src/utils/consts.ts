d2b1901f9ba705dfe8f833baa92ed40081629ffa
Adrian Apan <mail@adrianapan.com>
2023-04-19 12:58:24 +0100

    ✨ add consts file

---
+++ src/utils/consts.ts
@@ -0,0 +1,4 @@
+export const API_BASE_URL = 'https://61c32f169cfb8f0017a3e9f4.mockapi.io'
+
+export const DEFAULT_TITLE = 'Address book'
+export const DEFAULT_DESCRIPTION = 'An address book app built with Next JS'
