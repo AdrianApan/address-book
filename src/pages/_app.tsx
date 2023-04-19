86f85c38161b2626260f26f146c073bea5c9472d
Adrian Apan <mail@adrianapan.com>
2023-04-19 11:41:42 +0100

    ✨ add MUI theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import 'src/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme, ThemeProvider } from '@mui/material'
+import CssBaseline from '@mui/material/CssBaseline'
+
+const theme = createTheme()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
