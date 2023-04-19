07674573dcc0f4ce5942c5efb02044a891fdbd54
Adrian Apan <mail@adrianapan.com>
2023-04-19 20:05:41 +0100

    ✨ add grid container

--- src/components/ContactsList/ContactsList.tsx
+++ src/components/ContactsList/ContactsList.tsx
@@ -1,8 +1,9 @@
-import { Button } from '@mui/material'
+import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
 
-import { Contact } from 'src/types/Contact'
 import useGetContacts from 'src/hooks/useGetContacts'
 import useAddContact from 'src/hooks/useAddContact'
+import ContactCard from '../ContactCard'
 
 const ContactsList = () => {
   const { data: allContacts, isLoading, error } = useGetContacts()
@@ -19,15 +20,17 @@ const ContactsList = () => {
   const contactData = { name: 'xyz' }
 
   return (
-    <>
-      {allContacts?.map(({ id, name }: Contact) => (
-        <li key={id}>{name}</li>
+    <Grid container spacing={2}>
+      {allContacts?.map((contact) => (
+        <Grid key={contact.id} item xs={12} lg={4}>
+          <ContactCard details={contact} />
+        </Grid>
       ))}
 
-      <Button variant="contained" onClick={() => addContact.mutate({ data: contactData })}>
+      {/* <Button variant="contained" onClick={() => addContact.mutate({ data: contactData })}>
         ADD NEW
-      </Button>
-    </>
+      </Button> */}
+    </Grid>
   )
 }
 
