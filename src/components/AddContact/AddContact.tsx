a066466db2702e41fcad586310c72d2d9b404624
Adrian Apan <mail@adrianapan.com>
2023-04-20 12:18:29 +0100

    ✨ add contact comp

---
+++ src/components/AddContact/AddContact.tsx
@@ -0,0 +1,21 @@
+import dayjs from 'dayjs'
+import Button from '@mui/material/Button'
+import AddIcon from '@mui/icons-material/Add'
+
+import useAddContact from 'src/hooks/useAddContact'
+
+const AddContact = () => {
+  const addContact = useAddContact()
+
+  return (
+    <Button
+      variant="outlined"
+      onClick={() => addContact.mutate({ data: { createdAt: dayjs().toString() } })}
+      startIcon={<AddIcon />}
+    >
+      Add new contact
+    </Button>
+  )
+}
+
+export default AddContact
