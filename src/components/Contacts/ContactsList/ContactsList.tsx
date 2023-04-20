d5b125230ba2558090dd5091f7fc6b6837b06726
Adrian Apan <mail@adrianapan.com>
2023-04-20 12:36:16 +0100

    ✨ add no results callout

--- src/components/Contacts/ContactsList/ContactsList.tsx
+++ src/components/Contacts/ContactsList/ContactsList.tsx
@@ -16,26 +16,33 @@ const ContactsList = () => {
     <>
       <Search />
 
-      {error ? (
+      {error && (
         <Alert severity="error">
           <AlertTitle>Error</AlertTitle>
           {error?.message || DEFAULT_ERROR_MESSAGE}
         </Alert>
-      ) : (
-        <Grid container spacing={2}>
-          {isLoading
-            ? [...Array(9)].map((_, i) => (
-                <Grid key={i} item xs={12} lg={4}>
-                  <Skeleton variant="rectangular" width="100%" height={200} />
-                </Grid>
-              ))
-            : allContacts?.map((contact) => (
-                <Grid key={contact.id} item xs={12} lg={4}>
-                  <ContactCard details={contact} />
-                </Grid>
-              ))}
-        </Grid>
       )}
+
+      {!isLoading && !error && !allContacts?.length && (
+        <Alert severity="info">
+          <AlertTitle>No results</AlertTitle>
+          There are no contacts in your address book.
+        </Alert>
+      )}
+
+      <Grid container spacing={2}>
+        {isLoading
+          ? [...Array(9)].map((_, i) => (
+              <Grid key={i} item xs={12} lg={4}>
+                <Skeleton variant="rectangular" width="100%" height={200} />
+              </Grid>
+            ))
+          : allContacts?.map((contact) => (
+              <Grid key={contact.id} item xs={12} lg={4}>
+                <ContactCard details={contact} />
+              </Grid>
+            ))}
+      </Grid>
     </>
   )
 }
