952effd5c0c00fdf4e47b90ac469606fbf06ef40
Adrian Apan <mail@adrianapan.com>
2023-04-20 14:27:43 +0100

    ✨ add loading state on the CTAs

--- src/components/Contacts/AddContact/AddContact.tsx
+++ src/components/Contacts/AddContact/AddContact.tsx
@@ -1,17 +1,20 @@
 import dayjs from 'dayjs'
 import Button from '@mui/material/Button'
 import AddIcon from '@mui/icons-material/Add'
+import CircularProgress from '@mui/material/CircularProgress'
 
 import useAddContact from 'src/hooks/useAddContact'
 
 const AddContact = () => {
   const addContact = useAddContact()
+  const { isLoading } = addContact
 
   return (
     <Button
       variant="outlined"
       onClick={() => addContact.mutate({ data: { createdAt: dayjs().toString() } })}
-      startIcon={<AddIcon />}
+      startIcon={isLoading ? <CircularProgress size={16} color="info" /> : <AddIcon />}
+      disabled={isLoading}
     >
       Add
     </Button>
--- src/components/Contacts/ContactCard/ContactCard.tsx
+++ src/components/Contacts/ContactCard/ContactCard.tsx
@@ -5,6 +5,7 @@ import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardActions from '@mui/material/CardActions'
+import CircularProgress from '@mui/material/CircularProgress'
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined'
 import PhoneOutlinedIcon from '@mui/icons-material/PhoneOutlined'
 import CakeOutlinedIcon from '@mui/icons-material/CakeOutlined'
@@ -17,6 +18,7 @@ import useDeleteContact from 'src/hooks/useDeleteContact'
 const ContactCard = ({ details }: { details: Contact }) => {
   const { id, name, avatar, email, phone, birthday, createdAt } = details
   const deleteContact = useDeleteContact()
+  const { isLoading } = deleteContact
 
   return (
     <Card variant="outlined" sx={{ padding: 2 }}>
@@ -52,7 +54,16 @@ const ContactCard = ({ details }: { details: Contact }) => {
         <Button size="small" variant="outlined">
           Edit
         </Button>
-        <Button size="small" color="error" onClick={() => deleteContact.mutate({ id })}>
+        <Button
+          size="small"
+          color="error"
+          onClick={() => deleteContact.mutate({ id })}
+          disabled={isLoading}
+          {...(isLoading && {
+            variant: 'contained',
+            startIcon: <CircularProgress size={16} color="primary" />,
+          })}
+        >
           Delete
         </Button>
       </CardActions>
