bb4f8d475c97c8e679b0f96a10bccde3406c3b15
Adrian Apan <mail@adrianapan.com>
2023-04-21 13:05:30 +0100

    📦 add cypress

---
+++ cypress/support/selectors.ts
@@ -0,0 +1,9 @@
+const selectors = {
+  contactsGrid: '[data-cy="contacts-grid"]',
+  contactCard: '[data-cy="contact-card"]',
+  contactSearch: '[data-cy="contact-search"]',
+  errorBanner: '[data-cy="error-banner"]',
+  noResultsBanner: '[data-cy="no-results-banner"]',
+}
+
+export default selectors
